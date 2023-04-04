112e01763243da5a8ef04125d3c1e1d8fb64515c
Ayush gupta <ayush111195>
2023-04-05 02:52:33 +0530

    FINAL

--- Microservices-1/src/main/java/com/hotel/services/controller/HotelController.java
+++ Microservices-1/src/main/java/com/hotel/services/controller/HotelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ public class HotelController {
 private HotelService hotelService;	
 	//create ---->>
 
+@PreAuthorize("hasAuthority('SCOPE_internal')")   // YE API WAHI CALL KARWA PAYEGA JISKE PASS INTERNAL SCOP HO
 @PostMapping("/")
 public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
 	
